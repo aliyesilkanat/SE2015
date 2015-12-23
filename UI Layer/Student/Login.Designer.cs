87ed021f47dc58912a6d72d6da136c2beb59e1f1
Ali Yeşilkanat <aliyesilkanat@gmail.com>
2015-12-23 23:15:00 +0200

    merge

---
+++ UI Layer/Student/Login.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace SE2015.UI_Layer.Student
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kullanici_adi = new System.Windows.Forms.TextBox();
+            this.sifre = new System.Windows.Forms.TextBox();
+            this.uye_girisi = new System.Windows.Forms.Button();
+            this.Giris_Uyari = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.kayit_ol = new System.Windows.Forms.Button();
+            this.kayit_email = new System.Windows.Forms.TextBox();
+            this.kayit_kullanici_Adi = new System.Windows.Forms.TextBox();
+            this.kayit_sifre = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.kayit_Ad = new System.Windows.Forms.TextBox();
+            this.kayit_Soyad = new System.Windows.Forms.TextBox();
+            this.kayitUyari = new System.Windows.Forms.Label();
+            this.comboBox1_uyetipi = new System.Windows.Forms.ComboBox();
+            this.comboBox2_kuyetipi = new System.Windows.Forms.ComboBox();
+            this.uyeTipi = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(16, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Üye Girişi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kullanıcı Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(54, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Şifre:";
+            // 
+            // kullanici_adi
+            // 
+            this.kullanici_adi.BackColor = System.Drawing.SystemColors.Menu;
+            this.kullanici_adi.Location = new System.Drawing.Point(91, 102);
+            this.kullanici_adi.Name = "kullanici_adi";
+            this.kullanici_adi.Size = new System.Drawing.Size(121, 20);
+            this.kullanici_adi.TabIndex = 4;
+            // 
+            // sifre
+            // 
+            this.sifre.BackColor = System.Drawing.SystemColors.Menu;
+            this.sifre.Location = new System.Drawing.Point(91, 142);
+            this.sifre.Name = "sifre";
+            this.sifre.PasswordChar = '*';
+            this.sifre.Size = new System.Drawing.Size(121, 20);
+            this.sifre.TabIndex = 5;
+            // 
+            // uye_girisi
+            // 
+            this.uye_girisi.BackColor = System.Drawing.Color.ForestGreen;
+            this.uye_girisi.Location = new System.Drawing.Point(104, 234);
+            this.uye_girisi.Name = "uye_girisi";
+            this.uye_girisi.Size = new System.Drawing.Size(69, 29);
+            this.uye_girisi.TabIndex = 6;
+            this.uye_girisi.Text = "Giriş";
+            this.uye_girisi.UseVisualStyleBackColor = false;
+            this.uye_girisi.Click += new System.EventHandler(this.uye_girisi_Click);
+            // 
+            // Giris_Uyari
+            // 
+            this.Giris_Uyari.AutoSize = true;
+            this.Giris_Uyari.ForeColor = System.Drawing.Color.Red;
+            this.Giris_Uyari.Location = new System.Drawing.Point(88, 158);
+            this.Giris_Uyari.Name = "Giris_Uyari";
+            this.Giris_Uyari.Size = new System.Drawing.Size(0, 13);
+            this.Giris_Uyari.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label5.Location = new System.Drawing.Point(340, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kayıt Olmak İçin";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label9.Location = new System.Drawing.Point(340, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Kullanıcı Adı:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label7.Location = new System.Drawing.Point(376, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Şifre:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label8.Location = new System.Drawing.Point(370, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "e-mail:";
+            // 
+            // kayit_ol
+            // 
+            this.kayit_ol.BackColor = System.Drawing.Color.SteelBlue;
+            this.kayit_ol.Location = new System.Drawing.Point(440, 289);
+            this.kayit_ol.Name = "kayit_ol";
+            this.kayit_ol.Size = new System.Drawing.Size(69, 29);
+            this.kayit_ol.TabIndex = 10;
+            this.kayit_ol.Text = "Kayıt Ol";
+            this.kayit_ol.UseVisualStyleBackColor = false;
+            this.kayit_ol.Click += new System.EventHandler(this.kayit_ol_Click);
+            // 
+            // kayit_email
+            // 
+            this.kayit_email.BackColor = System.Drawing.SystemColors.Menu;
+            this.kayit_email.Location = new System.Drawing.Point(424, 95);
+            this.kayit_email.Name = "kayit_email";
+            this.kayit_email.Size = new System.Drawing.Size(121, 20);
+            this.kayit_email.TabIndex = 7;
+            // 
+            // kayit_kullanici_Adi
+            // 
+            this.kayit_kullanici_Adi.BackColor = System.Drawing.SystemColors.Menu;
+            this.kayit_kullanici_Adi.Location = new System.Drawing.Point(424, 126);
+            this.kayit_kullanici_Adi.Name = "kayit_kullanici_Adi";
+            this.kayit_kullanici_Adi.Size = new System.Drawing.Size(121, 20);
+            this.kayit_kullanici_Adi.TabIndex = 8;
+            // 
+            // kayit_sifre
+            // 
+            this.kayit_sifre.BackColor = System.Drawing.SystemColors.Menu;
+            this.kayit_sifre.Location = new System.Drawing.Point(424, 158);
+            this.kayit_sifre.Name = "kayit_sifre";
+            this.kayit_sifre.PasswordChar = '*';
+            this.kayit_sifre.Size = new System.Drawing.Size(121, 20);
+            this.kayit_sifre.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label10.Location = new System.Drawing.Point(384, 189);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 16);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Ad:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label11.Location = new System.Drawing.Point(371, 220);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 16);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Soyad";
+            // 
+            // kayit_Ad
+            // 
+            this.kayit_Ad.BackColor = System.Drawing.SystemColors.Menu;
+            this.kayit_Ad.Location = new System.Drawing.Point(424, 189);
+            this.kayit_Ad.Name = "kayit_Ad";
+            this.kayit_Ad.Size = new System.Drawing.Size(121, 20);
+            this.kayit_Ad.TabIndex = 17;
+            // 
+            // kayit_Soyad
+            // 
+            this.kayit_Soyad.BackColor = System.Drawing.SystemColors.Menu;
+            this.kayit_Soyad.Location = new System.Drawing.Point(424, 220);
+            this.kayit_Soyad.Name = "kayit_Soyad";
+            this.kayit_Soyad.Size = new System.Drawing.Size(121, 20);
+            this.kayit_Soyad.TabIndex = 18;
+            // 
+            // kayitUyari
+            // 
+            this.kayitUyari.AutoSize = true;
+            this.kayitUyari.ForeColor = System.Drawing.Color.Red;
+            this.kayitUyari.Location = new System.Drawing.Point(370, 267);
+            this.kayitUyari.Name = "kayitUyari";
+            this.kayitUyari.Size = new System.Drawing.Size(0, 13);
+            this.kayitUyari.TabIndex = 19;
+            // 
+            // comboBox1_uyetipi
+            // 
+            this.comboBox1_uyetipi.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox1_uyetipi.FormattingEnabled = true;
+            this.comboBox1_uyetipi.Items.AddRange(new object[] {
+            "Öğrenci",
+            "Öğretmen",
+            "Admin"});
+            this.comboBox1_uyetipi.Location = new System.Drawing.Point(91, 183);
+            this.comboBox1_uyetipi.Name = "comboBox1_uyetipi";
+            this.comboBox1_uyetipi.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1_uyetipi.TabIndex = 20;
+            // 
+            // comboBox2_kuyetipi
+            // 
+            this.comboBox2_kuyetipi.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox2_kuyetipi.FormattingEnabled = true;
+            this.comboBox2_kuyetipi.Items.AddRange(new object[] {
+            "Öğrenci",
+            "Öğretmen",
+            "Admin"});
+            this.comboBox2_kuyetipi.Location = new System.Drawing.Point(424, 259);
+            this.comboBox2_kuyetipi.Name = "comboBox2_kuyetipi";
+            this.comboBox2_kuyetipi.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2_kuyetipi.TabIndex = 21;
+            // 
+            // uyeTipi
+            // 
+            this.uyeTipi.AutoSize = true;
+            this.uyeTipi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.uyeTipi.ForeColor = System.Drawing.Color.ForestGreen;
+            this.uyeTipi.Location = new System.Drawing.Point(41, 184);
+            this.uyeTipi.Name = "uyeTipi";
+            this.uyeTipi.Size = new System.Drawing.Size(51, 16);
+            this.uyeTipi.TabIndex = 22;
+            this.uyeTipi.Text = "Seçiniz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(367, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Seçiniz";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::SE2015.Properties.Resources._4_ScienceRocks_01;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.uyeTipi);
+            this.Controls.Add(this.comboBox2_kuyetipi);
+            this.Controls.Add(this.comboBox1_uyetipi);
+            this.Controls.Add(this.kayitUyari);
+            this.Controls.Add(this.kayit_Soyad);
+            this.Controls.Add(this.kayit_Ad);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.kayit_sifre);
+            this.Controls.Add(this.kayit_kullanici_Adi);
+            this.Controls.Add(this.kayit_email);
+            this.Controls.Add(this.kayit_ol);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Giris_Uyari);
+            this.Controls.Add(this.uye_girisi);
+            this.Controls.Add(this.sifre);
+            this.Controls.Add(this.kullanici_adi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sifre;
+        private System.Windows.Forms.Button uye_girisi;
+        private System.Windows.Forms.Label Giris_Uyari;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button kayit_ol;
+        private System.Windows.Forms.TextBox kayit_email;
+        private System.Windows.Forms.TextBox kayit_sifre;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox kullanici_adi;
+        private System.Windows.Forms.TextBox kayit_kullanici_Adi;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox kayit_Ad;
+        private System.Windows.Forms.TextBox kayit_Soyad;
+
+        private System.Windows.Forms.Label kayitUyari;
+        private System.Windows.Forms.ComboBox comboBox1_uyetipi;
+        private System.Windows.Forms.ComboBox comboBox2_kuyetipi;
+        private System.Windows.Forms.Label uyeTipi;
+        private System.Windows.Forms.Label label4;
+    }
+}
